03296d34652cb9566ca266a44ccaf65baf3aa965
jumisz <juan.m.salamanca@gmail.com>
2012-05-19 20:56:40 +0100

    Adding more elements to data model

--- models/init.rb
+++ models/init.rb
@@ -8,3 +8,28 @@ DB.create_table? :pages do
   String :text
   String :image_file
 end
+
+DB.create_table? :menus do
+  primary_key :id
+  String :name
+  String :category
+  Integer :price
+  DateTime :modified
+end
+
+DB.create_table? :menuitems do
+  primary_key :id
+  foreign_key :menu_id, :menus
+  String :description
+  String :
+  String :image_file
+end
+
+
+DB.create_table? :dishes do
+  primary_key :id
+  String :section
+  String :title
+  String :text
+  String :image_file
+end
